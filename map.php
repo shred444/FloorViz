8f66388b2e6182063c0fc2d5f8033cbd66f67a7c
Jonathan Cohn <jcohn@kivasystems.com>
2012-10-24 15:03:07 -0400

    active selection accordian

--- map.php
+++ map.php
@@ -23,18 +23,110 @@
 <script>
 
 	$(function() {
-		$( "#accordion" ).accordion();
+		$( "#accordion" ).accordion({
+			autoHeight: true,
+			collapsible: true,
+			heightStyle: "content",
+			fillSpace: false
+		});
+		
+		//initialize slider
+		$( "#slider" ).slider({
+			range: true,
+			values: [ filter.duration.min, filter.duration.max ],	//default values
+			min: 1,
+			max: 100,
+			change: function( event, ui ) {
+				
+				filter.duration.min = ui.values[0];
+				filter.duration.max = ui.values[1];
+				
+				$( "#amount" ).val( filter.duration.min + " - " + filter.duration.max );
+				filterRefresh();
+			},
+			slide: function( event, ui ) {
+				var myMin = ui.values[0];
+				var myMax = ui.values[1];
+				$( "#amount" ).val( myMin + " - " + myMax );
+			}
+		});
+		
+		$( "#from" ).datepicker({
+            defaultDate: "+1w",
+            changeMonth: true,
+            numberOfMonths: 1,
+            onSelect: function( selectedDate ) {
+                //$( "#to" ).datepicker( "option", "minDate", selectedDate );
+				console.log("From date selected: " + selectedDate);
+				filter.timeRange.min = new Date(selectedDate);
+				filter.timeRange.min.setHours(0,0,0);
+				filterRefresh();
+            }
+        });
+        $( "#to" ).datepicker({
+            defaultDate: "+1w",
+            changeMonth: true,
+            numberOfMonths: 1,
+            onSelect: function( selectedDate ) {
+                //$( "#from" ).datepicker( "option", "maxDate", selectedDate );
+				console.log("To date selected: " + selectedDate);
+				filter.timeRange.max = new Date(selectedDate);
+				filter.timeRange.max.setHours(23,59,59);
+				filterRefresh();
+            }
+        });
 	});
 </script>
+<div id="header">
+	<form id="facility" action="roams.php" method="get">
+			
+		<ul style="list-style-type:none">
+			<li id="logo">
+				Facility:
+			</li>
+			<li>
+				<select id="site" name="site" onchange="populateDatasets('facility')">
+					<?php foreach ($databases as $db){ ?>
+						<option  <?php if($site == $db) echo "selected='selected'"; ?> value="<?php echo $db ?>"><?php echo $db ?></option>
+					<?php }?>
+												
+				</select>
+			</li>
+			<li>
+				<select id="dataset" name="dataset">
+				<?php
+					mysql_data_seek( $datasets, 0);
+					while($row = mysql_fetch_array($datasets))
+					{ ?>
+						<option <?php if($dataset == $row['name']) echo "selected='selected'"; ?> value="<?php echo $row['name']; ?>"><?php echo $row['name']; ?></option>
+					<?php }?>
+				</select>
+			</li>
+			
+			<li>
+			
+				<label for="from">From</label>
+				<input type="text" id="from" name="from" />
+				<label for="to">to</label>
+				<input type="text" id="to" name="to" />
+			</li>
+			<li>
+				<input type="submit" value="Refresh"> 
+			</li>
+			
+			
+		</ul>
+	</form>
+</div>
 
-<div id="sidebar" width="300">
-	<div id="accordion">
+<div id="sidebar2" style="position: relative; width:300px; float:right;">
+	<div id="accordion" style="position:relative;">
 		<h3>Selection</h3>
-			<div>Lorem ipsum dolor sit amet. Lore</div>
+			<div width="300" id="selectionTab">Lorem ipsum dolor sit amet. Lore</div>
 		<h3>Filter</h3>
-			<div>Phasellus mattis tincidunt nibh.</div>
+			<div width="300"><div id="slider"></div></div>
 		<h3>Details</h3>
-			<div>Nam dui erat, auctor a, dignissim quis.</div>
+			<div width="300">Nam dui erat, auctor a, dignissim quis.</div>
 	</div>
 </div>
 
